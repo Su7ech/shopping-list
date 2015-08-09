5d9f7966c7635f1a3aeb51b96f3d043c85c1096b
Su7ech <tpbaroody@gmail.com>
2015-08-09 11:45:08 -0700

    updated master branch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
-
+	addRemoveItemsToTheList();
 });
+
+function addRemoveItemsToTheList() {
+	var addItem = function() {
+		var inputItem = $('#item').val();
+		var createListItem = $('<li>');
+		var addToList = $('<span class="check"><i class="fa fa-check fa-lg"></i></span>' + inputItem + '<span class="remove"><i class="fa fa-times fa-lg"></i></span>');
+		if ( (inputItem) == '' ) {
+			alert("Looks like you didn't type anything");
+		} else {
+			createListItem.html(addToList).appendTo('.list');
+			$('.clear-list').css('display', 'block');
+			$('#how-to').show();
+		};
+		$('#item').val('');
+	};
+	$('#add').click(addItem);
+	$('#item').keypress(function(enter) {
+		if ( enter.keyCode == 13 ) {
+			addItem();
+		};
+	});
+	$('.clear-list').click(function() {
+		$('.list').empty();
+		$('.clear-list').css('display', 'none');
+		$('#how-to').hide();
+	});
+	$('.list').on('click', '.remove', function(event) {
+		event.preventDefault();
+		$('li').prev().remove();
+	});
+	$('.list').on('click', '.check', function(event) {
+		event.preventDefault();
+		$('li').css('text-decoration', 'line-through').css('background-color', 'gray');
+	});
+};
